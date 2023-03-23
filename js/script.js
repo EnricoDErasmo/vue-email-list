1c9374cd81d06dc2d276d87dcd50b96fe90960bf
EnricoDErasmo <enrico.derasmo@gmail.com>
2023-03-23 10:03:16 +0100

    bunus done

--- js/script.js
+++ js/script.js
@@ -5,13 +5,15 @@ createApp({
       return {
         // Creo un array vuoto
         emailList: [],
+        visibility: false,
       }
       
     },
 
     created(){
+
         // Creo un ciclo
-        for(i = 0; i < 10; i++){
+        for(let i = 0; i < 10; i++){
             // Trapite l'API faccio in modo che vengano generate 10 email casuali
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res) => {
     
@@ -19,9 +21,16 @@ createApp({
                 let newEmail = res.data.response;
                 // Inserisco ogni email all'interno dell'array
                 this.emailList.push(newEmail);
-            });
-            
+
+                if(this.emailList.length == 10){
+
+                    this.visibility = true;
+
+                }
+                
+            });  
         }
         console.log(this.emailList);
+
     }
   }).mount('#app')
